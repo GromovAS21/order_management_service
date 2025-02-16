7adcef0216971eea7a109274261fcce0c39db08f
Anton Gromov <seed14112@yandex.ru>
2025-02-16 15:04:49 +0300

    [+] Добавление в проект DRF

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 LANGUAGE_CODE = "ru-us"
 
