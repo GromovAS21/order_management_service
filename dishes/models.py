036e80ef49d61b90018f8e8ffbe6b918977d371a
Anton Gromov <seed14112@yandex.ru>
2025-02-17 22:38:16 +0300

    [+] Добавлен валидации для цены блюда

--- dishes/models.py
+++ dishes/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -6,7 +7,11 @@ class Dish(models.Model):
 
     name = models.CharField(unique=True, max_length=100, verbose_name="Названия блюда")
     price = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="Цена", help_text="Цена указывается в рублях"
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена",
+        help_text="Цена указывается в рублях",
+        validators=[MinValueValidator(0)],
     )
     description = models.TextField(verbose_name="Описание блюда", blank=True, null=True)
 
