2bbd83a3cbd80f6dfd80f4db46050d8406749215
Anton Gromov <seed14112@yandex.ru>
2025-02-17 16:04:29 +0300

    [+] Добавление пагинации

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 LOGGING = {
