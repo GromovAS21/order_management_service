cf8dad6e681f4f96f67c1bb8f792c67ad7791cc4
Anton Gromov <seed14112@yandex.ru>
2025-02-18 20:03:37 +0300

    [+] Добавление экрана создания заказа

---
+++ orders/forms.py
@@ -0,0 +1,32 @@
+from django.forms import ModelForm
+
+from orders.models import Order
+
+
+class StyleFormMixin:
+    """
+    Mixin для стилизации формы.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+
+class OrderForm(StyleFormMixin, ModelForm):
+    """Форма для создания заказа"""
+
+    class Meta:
+        model = Order
+        fields = ("table_number", "items")
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.serializer = OrderCreateSerializer(data=self.data)
+    #
+    # def is_valid(self):
+    #     return self.serializer.is_valid()
+    #
+    # def save(self, commit=True):
+    #     return self.serializer.save()
--- orders/view_html.py
+++ orders/view_html.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
+from orders.forms import OrderForm
 from orders.models import Order
+from orders.services import OrderService
 
 
 class OrderListView(ListView):
@@ -15,3 +19,17 @@ class OrderListView(ListView):
         context = super().get_context_data(**kwargs)
         context["number"] = [number for number in range(1, len(context["object_list"]) + 1)]
         return context
+
+
+class OrderCreateView(LoginRequiredMixin, CreateView):
+    """Контроллер для создания заказа"""
+
+    model = Order
+    form_class = OrderForm
+    success_url = reverse_lazy("orders:orders_list_html")
+
+    def form_valid(self, form):
+        order = form.save()
+        order.total_price = OrderService.calculate_total_price(order)
+        order.save(update_fields=["total_price"])
+        return super().form_valid(form)
