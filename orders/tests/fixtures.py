7630209af5ee122851ead58857197bdf4cb717db
Anton Gromov <seed14112@yandex.ru>
2025-02-20 13:54:33 +0300

    [+] Добавление тестов для заказов и установка зависимости coverage

--- orders/tests/fixtures.py
+++ orders/tests/fixtures.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django.contrib.auth.models import User
 
-from orders.models import CHOICES_QUANTITY_SEAT, Table
+from dishes.tests.fixtures import create_test_dish
+from orders.models import CHOICES_QUANTITY_SEAT, Order, Table
 
 
 def create_test_user():
@@ -11,3 +14,11 @@ def create_test_user():
 def create_test_table():
     """Создание тестового стола"""
     return Table.objects.create(number=2, quantity_seat=CHOICES_QUANTITY_SEAT.ONE)
+
+
+def create_test_order(table, dish):
+    """Создание тестового заказа"""
+    today_date = datetime.datetime.now()
+    order = Order.objects.create(table_number=table, total_price="100.00", order_datetime=today_date)
+    order.items.add(dish)
+    return order
