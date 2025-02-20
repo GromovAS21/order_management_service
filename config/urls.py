7901749d71c6b69ceb772b9212671da7264ee02a
Anton Gromov <seed14112@yandex.ru>
2025-02-20 15:26:48 +0300

    [+] Добавление картинки на стартовый экран

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+from config import settings
 
 
 schema_view = get_schema_view(
@@ -26,3 +29,6 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
